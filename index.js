217e1c8782e92800c66fb00d3757d2a75868b4a4
A01027987 <jgarricks@my.bcit.ca>
2019-03-03 19:33:00 -0800

    l

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var http = require('http');
 var xmldom = require('xmldom');
 const parse = require('csv-parse');
 var root = '';
+var root2 = '';
 var userinput = process.argv[2].toUpperCase();
 const weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri']
 const status = "Active"
@@ -11,17 +12,20 @@ const status = "Active"
 http.createServer(function (req, res) {
 	parser = new xmldom.DOMParser();
 	xmldoc = parser.parseFromString(`<classes></classes>`, 'text/xml');
+	xmldoc2 = parser.parseFromString(`<classes></classes>`, 'text/xml');
 	root = xmldoc.documentElement;
+	root2 = xmldoc2.documentElement;
 	res.writeHead(200, { 'Content-Type': 'text/html' });
 	result = '';
-	x = root.childNodes;
+	student = root.childNodes;
+	teacher = root2.childNodes;
 
 	const input = fs.readFileSync('data/Subject_Course_Timetables.csv')
 	const output = []
 	parse(input, {
 		trim: true,
 		skip_empty_lines: true,
-		from_line: 2
+		from_line: 17
 	})
 		.on('readable', function () {
 			let record
@@ -30,7 +34,8 @@ http.createServer(function (req, res) {
 				if (record[0] == status) {
 					if (weekdays.indexOf(record[5]) > -1) {
 						//creates the xml
-						makeXml(record, userinput)
+						makeStudentXml(record, userinput)
+						makeTeacherXml(record, userinput)
 					} else {
 					}
 				} else {
@@ -41,10 +46,19 @@ http.createServer(function (req, res) {
 		.on('end', function () {
 			serializer = new xmldom.XMLSerializer();
 			tosave = serializer.serializeToString(xmldoc);
+			tosave2 = serializer.serializeToString(xmldoc2);
 			//adds the xml version and doctipe tags
 			tosave=`<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE classes SYSTEM "classes.dtd"> ${tosave}`;
+			//tosave=`<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE classes SYSTEM "classes.dtd"> ${tosave}`;
 			fs.writeFileSync(`./data/201830-${userinput}.xml`, tosave);
-			result = show(x);
+			fs.writeFileSync(`./data/201830-${userinput}-Teacher.xml`, tosave2);
+			if(req.url == '/teachers'){
+				result = showTeacher(teacher)
+			}
+			else{
+				result = showStudent(student);
+			}
+			
 			//console.log(result)
 			res.write(result);
 			res.end();
@@ -53,11 +67,11 @@ http.createServer(function (req, res) {
 
 }).listen(8080); //the server object listens on port 8080 
 
-function show(data) {
+function showStudent(data) {
 	for (i = 0; i < data.length; i++) {
-		sub = x[i].childNodes
+		sub = data[i].childNodes
 		//adds the headder
-		result += `<h2>${x[i].nodeName}</h2><ul>`
+		result += `<h2>${data[i].nodeName}</h2><ul>`
 		for (n = 0; n < sub.length; n++) {
 			courses = sub[n].childNodes
 			//adds a line in the list
@@ -69,7 +83,7 @@ function show(data) {
 }
 
 
-function makeXml(data, course) {
+function makeStudentXml(data, course) {
 	//makes sure it is the right course / program (acit ect.)
 	if (data[1].substring(0, 4) == course) {
 		//checks if there is a existing set for the data if not it makes one
@@ -133,6 +147,72 @@ function makeXml(data, course) {
 			course.appendChild(hrs)
 			course.appendChild(teacher)
 			root.getElementsByTagName(`set_${data[1].substring(5, 8).replace(/ /g, "_")}`)[0].appendChild(course);
+		}
+	}
+}
+
+function makeTeacherXml(data, course) {
+	//makes sure it is the right course / program (acit ect.)
+	if (data[1].substring(0, 4) == course && data[8].length > 2) {
+		console.log(data[8])
+		//checks if there is a existing teacher catagory for the data if not it makes one
+		if (root2.getElementsByTagName(`${data[8].replace(/\*|\,|\'/g, '').trim().replace(/ /g, '_')}`)[0] == undefined) {
+			newItem = xmldoc2.createElement(`${data[8].replace(/\*|\,|\'/g, '').trim().replace(/ /g, '_')}`);
+			root2.appendChild(newItem)
+		}
+		z=0
+		make=true
+		//checks if there is already an existing couse (some courses have multiple sets) add the teacher to the couse if true
+		//  .replace(/ /g, "_") turns spaces into _'s as they are better for the xml element names
+		// while(z<root.getElementsByTagName(`${data[8].replace(/\*/g, '').trim()}`)[0].childNodes.length){
+		// 	if(root.getElementsByTagName(`${data[8].replace(/\*/g, '').trim()}`)[0].childNodes[z].childNodes[0].childNodes[0].nodeValue == data[3] && 
+		// 	root.getElementsByTagName(`${data[8].replace(/\*/g, '').trim()}`)[0].childNodes[z].childNodes[2].childNodes[0].nodeValue == data[6] &&
+		// 	root.getElementsByTagName(`${data[8].replace(/\*/g, '').trim()}`)[0].childNodes[z].childNodes[1].childNodes[0].nodeValue == data[5] ){
+		// 		teacher = xmldoc2.createElement('Teacher');
+		// 		teacher.textContent = data[8].trim();
+		// 		root.getElementsByTagName(`${data[8].replace(/\*/g, '').trim()}`)[0].childNodes[z].appendChild(teacher)
+		// 		make = false;
+		// 		break;
+		// 	}
+		// 	z++
+		// }
+		if(make == true){
+			//create elements
+			course = xmldoc2.createElement('Course');
+			name = xmldoc2.createElement('Name');
+			day = xmldoc2.createElement('Day');
+			sTime = xmldoc2.createElement('Start_Time');
+			eTime = xmldoc2.createElement('End_Time');
+			sDate = xmldoc2.createElement('Start_Date');
+			eDate = xmldoc2.createElement('End_Date');
+			room = xmldoc2.createElement('Room');
+			max = xmldoc2.createElement('Max');
+			act = xmldoc2.createElement('Act');
+			hrs = xmldoc2.createElement('Hrs');
+			//add text to elements
+			//  .replace(/\*/g, '').trim()  is used to remove *'s and leading and trailing whitespace
+			name.textContent = data[3].replace(/\*/g, '').trim();
+			day.textContent = data[5].replace(/\*/g, '').trim();
+			sTime.textContent = data[6].replace(/\*/g, '').trim();
+			eTime.textContent = data[7].replace(/\*/g, '').trim();
+			sDate.textContent = data[10].replace(/\*/g, '').trim();
+			eDate.textContent = data[11].replace(/\*/g, '').trim();
+			room.textContent = data[9].replace(/\*/g, '').trim();
+			max.textContent = data[12].replace(/\*/g, '').trim();
+			act.textContent = data[13].replace(/\*/g, '').trim();
+			hrs.textContent = data[14].replace(/\*/g, '').trim();
+			//append to root
+			course.appendChild(name)
+			course.appendChild(day)
+			course.appendChild(sTime)
+			course.appendChild(eTime)
+			course.appendChild(sDate)
+			course.appendChild(eDate)
+			course.appendChild(room)
+			course.appendChild(max)
+			course.appendChild(act)
+			course.appendChild(hrs)
+			root2.getElementsByTagName(`${data[8].replace(/\*|\,|\'/g, '').trim().replace(/ /g, '_')}`)[0].appendChild(course);
 		}
 	}
 }
